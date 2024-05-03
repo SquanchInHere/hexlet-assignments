ac0569aa49d1c74c59220d8367c1a332e0b34ec9
@hexlet/cli <support@hexlet.io>
2024-05-03 16:18:34 +0300

    submit rails-real-ru/system-tests

---
+++ rails-real-ru/system-tests/test/system/posts_test.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class PostTest < ApplicationSystemTestCase
+  setup do
+    @post = posts(:two)
+
+    @attrs = {
+      title: Faker::Book.title,
+      body: Faker::Books::Dune.quote
+    }
+  end
+
+  test 'visiting the index' do
+    visit posts_url
+
+    assert_selector 'h1', text: 'Posts'
+  end
+
+  test 'create a post' do
+    visit posts_url
+
+    click_on 'New Post'
+
+    fill_in 'Title', with: @attrs[:title]
+    fill_in 'Body', with: @attrs[:body]
+
+    click_on 'Create Post'
+
+    assert_text 'Post was successfully created'
+  end
+
+  test'show post' do
+    visit posts_url
+
+    within('table tbody tr:first-child') do
+      click_on 'Show'
+    end
+
+    assert_selector 'h1', text: @post.title
+  end
+
+  test 'add post comment' do
+    visit posts_url
+
+    within('table tbody tr:first-child') do
+      click_on 'Show'
+    end
+
+    fill_in 'post_comment[body]', with: 'Test Comments'
+
+    click_on 'Create Comment'
+
+    assert_text 'Comment was successfully created.'
+  end
+
+  test 'update a post' do
+    visit posts_url
+
+    within('table tbody tr:first-child') do
+      click_on 'Edit'
+    end
+
+    fill_in 'Title', with: Faker::Book.title
+    click_on 'Update Post'
+
+    assert_text 'Post was successfully updated'
+  end
+end
