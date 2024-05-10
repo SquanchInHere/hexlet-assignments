6da81396fcc6327cda316db8d87d5e972e52ffca
@hexlet/cli <support@hexlet.io>
2024-05-10 20:29:34 +0300

    submit rails-real-ru/controller-hierarchy

---
+++ rails-real-ru/controller-hierarchy/config/routes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  scope module: :web do
+    root 'home#index'
+
+    resources :movies do
+      resources :reviews
+      scope module: :movies do
+        resources :comments
+      end
+    end
+
+    resources :reviews, only: %i[index]
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
